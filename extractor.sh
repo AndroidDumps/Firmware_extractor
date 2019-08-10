3dfa6c5d18cbda021db7b627b3726c98b7004ac0
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-10 20:00:11 +0530

    Add support for *.bin seen in KDDI brand
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- extractor.sh
+++ extractor.sh
@@ -56,7 +56,7 @@ if [[ $MAGIC == "OPPOENCRYPT!" ]]; then
     exit
 fi
 
-if [[ ! $(7z l -ba $romzip | grep ".*system.ext4.tar.*\|.*.tar\|.*chunk\|system\/build.prop\|system.new.dat\|system_new.img\|system.img\|payload.bin\|image.*.zip\|update.zip\|.*rawprogram*\|system.sin" | grep -v ".*chunk.*\.so$") ]]; then
+if [[ ! $(7z l -ba $romzip | grep ".*system.ext4.tar.*\|.*.tar\|.*chunk\|system\/build.prop\|system.new.dat\|system_new.img\|system.img\|payload.bin\|image.*.zip\|update.zip\|.*rawprogram*\|system.sin\|system.*.bin" | grep -v ".*chunk.*\.so$") ]]; then
     echo "BRUH: This type of firmwares not supported"
     cd "$LOCALDIR"
     rm -rf "$tmpdir" "$outdir"
@@ -221,6 +221,14 @@ elif [[ $(7z l -ba $romzip | grep payload.bin) ]]; then
     rm payload.bin
     rm -rf "$tmpdir"
     exit
+elif [[ $(7z l -ba $romzip | grep "system.*.bin") ]]; then
+    echo "Update bin firmware detected"
+    7z x -y $romzip -o"$tmpdir"
+    bin_list=`find "$tmpdir" -type f -name "*.bin" -printf '%P\n' | sort`
+    for file in $bin_list; do
+        DIR_NAME=$(echo $file | cut -d . -f1)
+        7z x -y "$tmpdir/$file" -o"$outdir/$DIR_NAME"
+    done
 elif [[ $(7z l -ba $romzip | grep "image.*.zip\|update.zip") ]]; then
     echo "Image zip firmware detected"
     thezip=$(7z l -ba $romzip | grep "image.*.zip\|update.zip" | rev | gawk '{ print $1 }' | rev)
