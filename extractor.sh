fd56da45bae44c5a5a2341e229d35de38fa231d0
Erfan Abdi <erfangplus@gmail.com>
2019-08-10 16:18:57 +0430

    Support update.zip
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- extractor.sh
+++ extractor.sh
@@ -55,7 +55,7 @@ if [[ $MAGIC == "OPPOENCRYPT!" ]]; then
     exit
 fi
 
-if [[ ! $(7z l -ba $romzip | grep ".*system.ext4.tar.*\|.*.tar\|.*chunk\|system\/build.prop\|system.new.dat\|system_new.img\|system.img\|payload.bin\|image.*.zip\|.*rawprogram*\|system.sin" | grep -v ".*chunk.*\.so$") ]]; then
+if [[ ! $(7z l -ba $romzip | grep ".*system.ext4.tar.*\|.*.tar\|.*chunk\|system\/build.prop\|system.new.dat\|system_new.img\|system.img\|payload.bin\|image.*.zip\|update.zip\|.*rawprogram*\|system.sin" | grep -v ".*chunk.*\.so$") ]]; then
     echo "BRUH: This type of firmwares not supported"
     cd "$LOCALDIR"
     rm -rf "$tmpdir" "$outdir"
@@ -220,13 +220,12 @@ elif [[ $(7z l -ba $romzip | grep payload.bin) ]]; then
     rm payload.bin
     rm -rf "$tmpdir"
     exit
-elif [[ $(7z l -ba $romzip | grep "image.*.zip") ]]; then
+elif [[ $(7z l -ba $romzip | grep "image.*.zip\|update.zip") ]]; then
     echo "Image zip firmware detected"
-    thezip=$(7z l -ba $romzip | grep "image.*.zip" | rev | gawk '{ print $1 }' | rev)
+    thezip=$(7z l -ba $romzip | grep "image.*.zip\|update.zip" | rev | gawk '{ print $1 }' | rev)
     7z e -y $romzip $thezip 2>/dev/null >> $tmpdir/zip.log
     thezipfile=$(echo $thezip | rev | cut -d "/" -f 1 | rev)
-    mv $thezipfile temp.zip
-    "$LOCALDIR/extractor.sh" temp.zip "$outdir"
+    "$LOCALDIR/extractor.sh" "$thezipfile" "$outdir"
     exit
 fi
 
