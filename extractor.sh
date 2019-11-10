a19a2c7d738fc1c32f105b6368c0a34acb3e6f9c
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-11 04:09:01 +0530

    extractor: accept *.zip instead for Image zip firmware detection
    * some walton firmware are stored in different zip name
     * so accept all zip. this is last if else block anyways
    
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- extractor.sh
+++ extractor.sh
@@ -66,7 +66,7 @@ if [[ $MAGIC == "OPPOENCRYPT!" ]] || [[ "$romzipext" == "ozip" ]]; then
     exit
 fi
 
-if [[ ! $(7z l -ba $romzip | grep ".*system.ext4.tar.*\|.*.tar\|.*chunk\|system\/build.prop\|system.new.dat\|system_new.img\|system.img\|payload.bin\|image.*.zip\|update.zip\|.*rawprogram*\|system.sin\|system-p\|super\|UPDATE.APP\|.*.pac" | grep -v ".*chunk.*\.so$") ]]; then
+if [[ ! $(7z l -ba $romzip | grep ".*system.ext4.tar.*\|.*.tar\|.*chunk\|system\/build.prop\|system.new.dat\|system_new.img\|system.img\|payload.bin\|.*.zip\|.*rawprogram*\|system.sin\|system-p\|super\|UPDATE.APP\|.*.pac" | grep -v ".*chunk.*\.so$") ]]; then
     echo "BRUH: This type of firmwares not supported"
     cd "$LOCALDIR"
     rm -rf "$tmpdir" "$outdir"
@@ -275,7 +275,7 @@ elif [[ $(7z l -ba $romzip | grep payload.bin) ]]; then
     rm payload.bin
     rm -rf "$tmpdir"
     exit
-elif [[ $(7z l -ba $romzip | grep "image.*.zip\|update.zip") ]]; then
+elif [[ $(7z l -ba $romzip | grep ".*.zip") ]]; then
     echo "Image zip firmware detected"
     mkdir -p $tmpdir/zipfiles
     7z e -y $romzip -o$tmpdir/zipfiles 2>/dev/null >> $tmpdir/zip.log
