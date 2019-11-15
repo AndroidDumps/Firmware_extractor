ffdcbec1ee90fe89ea4de142945182e9d4ca6457
Erfan Abdi <erfangplus@gmail.com>
2019-11-15 16:21:52 +0330

    directly extract payload to out dir

--- extractor.sh
+++ extractor.sh
@@ -253,10 +253,7 @@ elif [[ $(7z l -ba $romzip | grep payload.bin) ]]; then
     echo "AB OTA detected"
     7z e -y $romzip payload.bin 2>/dev/null >> $tmpdir/zip.log
     for partition in $PARTITIONS; do
-        python $payload_extractor payload.bin --partitions $partition --output_dir $tmpdir > $tmpdir/extract.log
-        if [[ -f "$tmpdir/$partition" ]]; then
-            mv "$tmpdir/$partition" "$outdir/$partition.img"
-        fi
+        python $payload_extractor payload.bin --partitions $partition --output_dir $outdir > $tmpdir/extract.log
     done
     rm payload.bin
     rm -rf "$tmpdir"
