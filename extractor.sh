bc32fa9d75927bef7a7d40a3760249f6e05c6aeb
Erfan Abdi <erfangplus@gmail.com>
2019-11-30 13:21:09 +0330

    suppress md5 tar errors

--- extractor.sh
+++ extractor.sh
@@ -214,6 +214,9 @@ elif [[ $(7z l -ba $romzip | grep tar.md5 | gawk '{ print $NF }' | grep AP_) ]];
     cscmd5=$(7z l -ba $romzip | grep tar.md5 | gawk '{ print $NF }' | grep CSC_ | sed 's|.*/||')
     echo "Extracting images..."
     for i in "$mainmd5" "$cscmd5"; do
+        if [ ! -f "$i" ]; then
+            continue
+        fi
         for partition in $PARTITIONS; do
             tarulist=$(tar -tf $i | grep -e ".*$partition.*\.img.*\|.*$partition.*ext4")
             echo "$tarulist" | while read line; do
