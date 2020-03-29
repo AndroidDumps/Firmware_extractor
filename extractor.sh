48edf9fd938248135d7168b39c89f14e19faf4bd
Akhil Narang <me@akhilnarang.dev>
2020-03-30 00:10:57 +0530

    extractor: add some more partitions found in pixel r preview
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- extractor.sh
+++ extractor.sh
@@ -88,7 +88,7 @@ ruu="$toolsdir/$HOST/bin/RUU_Decrypt_Tool"
 
 romzip="$(realpath $1)"
 romzipext=${romzip##*.}
-PARTITIONS="system vendor cust odm oem factory product xrom modem dtbo boot tz systemex oppo_product preload_common"
+PARTITIONS="system vendor cust odm oem factory product xrom modem dtbo boot tz systemex oppo_product preload_common system_ext system_other"
 EXT4PARTITIONS="system vendor cust odm oem factory product xrom systemex oppo_product preload_common"
 OTHERPARTITIONS="tz.mbn:tz tz.img:tz modem.img:modem NON-HLOS:modem boot-verified.img:boot dtbo-verified.img:dtbo"
 
